11f3633d79d609ec50361fd4cad3f9d7126f1b38
JanWickerath <jan.wickerath@mailbox.org>
2016-11-15 17:03:01 +0100

    Initialization.

---
+++ laffer_curve.m
@@ -0,0 +1,11 @@
+% Initialize stuff
+w = 1;
+siggma = 2;
+cchi = 3;
+transfer = .5;                          % Nowhere stated note that transfers
+                                        % are endogenously determined by the
+                                        % households labour supply, but
+                                        % treated as exogenous in his
+                                        % optimization problem (i.e. taken
+                                        % as given when maximizing utility).
+tau = .5;
