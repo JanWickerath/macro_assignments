2aeb1851525ca9643010b885e4237ccdea37c670
JanWickerath <jan.wickerath@mailbox.org>
2016-11-15 17:34:15 +0100

    Loop over different tax rates.

--- laffer_curve.m
+++ laffer_curve.m
@@ -2,7 +2,7 @@
 w = 1;
 siggma = 2;
 cchi = 3;
-tau = .5;
+TaxRates = linspace(0.01, 0.99, 99);
 transfer = .5;                  % Nowhere stated note that transfers
                                 % are endogenously determined by the
                                 % households labour supply, but
@@ -10,8 +10,15 @@ transfer = .5;                  % Nowhere stated note that transfers
                                 % optimization problem (i.e. taken
                                 % as given when maximizing utility).
 
-% Use fsolve to find root of the system of FOCs over the choice of
-% consumption and hours
-% Create new function where FOCs are fixed at initialized values
-focForTau = @(x) SimTaxModelFOC(x, w, tau, transfer, siggma, cchi);
-optPol = fsolve(focForTau, [.5, .5])
+
+policy = nan(length(TaxRates), 2);
+idx = 1;
+for tau = TaxRates
+    % Use fsolve to find root of the system of FOCs over the choice of
+    % consumption and hours
+    % Create new function where FOCs are fixed at initialized values
+    focForTau = @(x) SimTaxModelFOC(x, w, tau, transfer, siggma, cchi);
+    policy(idx, :) = fsolve(focForTau, [.5, .5]);
+    idx = idx + 1;
+end
+policy
