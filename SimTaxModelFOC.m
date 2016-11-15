02486b939ac7e8a2150169af7c76070e4ae7654a
JanWickerath <jan.wickerath@mailbox.org>
2016-11-15 17:19:29 +0100

    Vectorize choice variables.

--- SimTaxModelFOC.m
+++ SimTaxModelFOC.m
@@ -1,4 +1,6 @@
-function F = SimTaxModelFOC(cons, hours, wage, tax, trans, sigg, cchi)
+function F = SimTaxModelFOC(x, wage, tax, trans, sigg, cchi)
+    cons = x(1);
+    hours = x(2);
     F(1) = (wage * hours * (1 - tax) + trans)^(-sigg) * wage * (1 - tax) - ...
            hours^cchi;
     F(2) = wage * hours * (1 - tax) + trans - cons;
