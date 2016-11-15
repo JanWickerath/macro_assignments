075c91a3b54b470cdebabad4cc98438951b42514
JanWickerath <jan.wickerath@mailbox.org>
2016-11-15 17:20:22 +0100

    Implement first version of policy function calculation for single taxrate.

--- laffer_curve.m
+++ laffer_curve.m
@@ -2,10 +2,16 @@
 w = 1;
 siggma = 2;
 cchi = 3;
-transfer = .5;                          % Nowhere stated note that transfers
-                                        % are endogenously determined by the
-                                        % households labour supply, but
-                                        % treated as exogenous in his
-                                        % optimization problem (i.e. taken
-                                        % as given when maximizing utility).
 tau = .5;
+transfer = .5;                  % Nowhere stated note that transfers
+                                % are endogenously determined by the
+                                % households labour supply, but
+                                % treated as exogenous in his
+                                % optimization problem (i.e. taken
+                                % as given when maximizing utility).
+
+% Use fsolve to find root of the system of FOCs over the choice of
+% consumption and hours
+% Create new function where FOCs are fixed at initialized values
+focForTau = @(x) SimTaxModelFOC(x, w, tau, transfer, siggma, cchi);
+optPol = fsolve(focForTau, [.5, .5])
