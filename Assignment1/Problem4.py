e9988581d88d7a752efe42d618d6934720ce751c
JanWickerath <jan.wickerath@mailbox.org>
2016-11-24 16:42:04 +0100

    outline helper functions.

---
+++ Assignment1/Problem4.py
@@ -0,0 +1,50 @@
+"""
+Solution to Problem 4: Find optimal linear taxation scheme in a world with
+under Greenwood, Hercowitz, Huffman preferences and wage heterogeneity among
+households.
+
+"""
+
+# Import scientific libraries
+from scipy.optimize import fsolve
+import scipy.integrate as integrate
+from numpy.random import lognormal
+
+class GovProblem():
+    """
+    Definition of the governments problem plus several methods.
+
+    """
+
+    # def __init__(self):
+    #     """
+    #     Create an instance of the government with a given social welfare
+    #     fucntion.
+    #     """
+
+    #     self.__swf__ = swf
+
+    # def set_swf(self, swf):
+    #     """
+    #     Method to set the governments social welfare function.
+
+    #     """
+    #     self.__swf__ = swf
+
+    # def foc(self, policies, wage_dist, psi):
+    #     """
+    #     First order condition of the government given the households optimal
+    #     policy decisions, a wage distribution and parameter psi.
+
+    #     """
+    #     tax = policies[0]
+    #     trans = policies[1]
+    #     def hours(wage, tax, psi):
+    #         return( ((1 - tax) * wage)**(1 / psi))
+    #     def cons(wage, tax, psi, trans):
+    #         return( ((1 - tax) * wage)**((1 + psi) / psi) + trans)
+
+# def hours(wage, tax, psi):
+#     return( ((1 - tax) * wage)**(1 / psi))
+# def cons(wage, tax, psi, trans):
+#     return( ((1 - tax) * wage)**((1 + psi) / psi) + trans)
