d15a864a011c0f7e4b16271cbdebe3e8e39836c1
JanWickerath <jan.wickerath@mailbox.org>
2016-11-24 17:48:28 +0100

    switch order of helper and class outline.

--- Assignment1/Problem4.py
+++ Assignment1/Problem4.py
@@ -8,13 +8,45 @@ households.
 # Import scientific libraries
 from scipy.optimize import fsolve
 import scipy.integrate as integrate
-import numpy.random.lognormal as np.lognormal
+from scipy.stats import lognorm
+import numpy as np
 
-class GovProblem():
-    """
-    Definition of the governments problem plus several methods.
 
-    """
+def hours(wage, tax, psi):
+    return( ((1 - tax) * wage)**(1 / psi))
+
+def cons(wage, tax, psi, trans):
+    return( ((1 - tax) * wage)**((1 + psi) / psi) + trans)
+
+def dell_u_tax(wage, cons, hours, psi, tax):
+    numerator = wage * hours
+    denom = cons - (hours**(1 + psi) / (1 + psi))
+    bracks = ((1 - tax) * wage)**((1 - psi) / psi) - (1 + psi) / psi
+    return(numerator / denom * bracks)
+
+def dell_u_trans(cons, hours, psi):
+    return(1 / (cons - hours**(1 + psi) / (1 + psi)))
+
+def foc(gov_policies, psi, sig):
+    tax = gov_policies[0]
+    trans = gov_policies[1]
+    part_a = integrate.quad(
+        lambda w: dell_u_tax(w, cons(w, tax, psi, trans),
+                             hours(w, tax, psi), psi, tax) * lognorm.pdf(
+                                 w, s=sig, scale=np.exp(-sig**2 / 2)),
+        0, 10                   # set integration borders
+    )
+
+    return part_a
+
+print(foc([0.5, 0.5], 2, 0.5))
+
+
+# class GovProblem():
+#     """
+#     Definition of the governments problem plus several methods.
+
+#     """
 
     # def __init__(self):
     #     """
@@ -43,8 +75,3 @@ class GovProblem():
     #         return( ((1 - tax) * wage)**(1 / psi))
     #     def cons(wage, tax, psi, trans):
     #         return( ((1 - tax) * wage)**((1 + psi) / psi) + trans)
-
-def hours(wage, tax, psi):
-    return( ((1 - tax) * wage)**(1 / psi))
-def cons(wage, tax, psi, trans):
-    return( ((1 - tax) * wage)**((1 + psi) / psi) + trans)
