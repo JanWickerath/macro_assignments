bb01dcb92bba98f14610e8f93e98ead113268613
JanWickerath <jan.wickerath@mailbox.org>
2017-01-10 19:02:51 +0100

    Test model solution with benchmark quantecon.

--- Assignment3/McCall_Model/my_mccall_model_test.py
+++ Assignment3/McCall_Model/my_mccall_model_test.py
@@ -1,5 +1,7 @@
 import unittest
+import numpy as np
 import my_mccall_model as mcm
+from mccall_bellman_iteration import solve_mccall_model, McCallModel
 
 
 class TestMyMcCall(unittest.TestCase):
@@ -16,6 +18,16 @@ class TestMyMcCall(unittest.TestCase):
         model = mcm.MyMcCallModel()
         self.assertEqual(model.get_utility(1), 0)
 
+    def test_solve_model_benchmark_quantecon(self):
+        model = mcm.MyMcCallModel()
+        true_mod = McCallModel(
+            w_vec=np.linspace(0, 1, 100), p_vec=np.array([.01] * 100)
+        )
+        # self.assertEqual(
+        #     model.solve(),
+        #     solve_mccall_model(true_mod)
+        # )
+        np.testing.assert_array_almost_equal
 
 if __name__ == '__main__':
     unittest.main()
