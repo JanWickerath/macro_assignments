1fec4dbac72ac97bccf51e448f45f3ffe748c649
JanWickerath <jan.wickerath@mailbox.org>
2017-01-10 18:33:14 +0100

    Add testcases for utility function.

--- Assignment3/McCall_Model/my_mccall_model_test.py
+++ Assignment3/McCall_Model/my_mccall_model_test.py
@@ -1,5 +1,21 @@
 import unittest
-import my_mccall_model
+import my_mccall_model as mcm
 
 
 class TestMyMcCall(unittest.TestCase):
+
+    def test_utility_zero_cons(self):
+        model = mcm.MyMcCallModel()
+        self.assertEqual(model.get_utility(0), -10e6)
+
+    def test_utility_log_case(self):
+        model = mcm.MyMcCallModel(sigma=1.0)
+        self.assertEqual(model.get_utility(1), 0)
+
+    def test_utility_sigma2(self):
+        model = mcm.MyMcCallModel()
+        self.assertEqual(model.get_utility(1), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
