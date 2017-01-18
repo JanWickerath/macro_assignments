066cbd0f8b2751f03370b6dc0b3527ebde6c1d0b
JanWickerath <jan.wickerath@mailbox.org>
2017-01-18 13:49:11 +0100

    Set up initial class of bewley model.

---
+++ Assignment3/Ayiagari/my_bewley_model.py
@@ -0,0 +1,31 @@
+"""
+Set up a bewley model class to model the householod sector of an Aiyagari
+style economy.
+
+"""
+
+# import numpy as np
+
+
+class MyBewleyModel():
+    """docstring for MyBewleyModel."""
+    def __init__(self, r):
+        pass
+
+    def _create_shock_process(self):
+        pass
+
+    def _utility(self):
+        pass
+
+    def _solve(self):
+        pass
+
+    def _create_transition(self):
+        pass
+
+    def _compute_stat_dist(self):
+        pass
+
+    def _aggregates(self):
+        pass
