caffa27133db45dac61bc7c5ef68df77f782daa6
JanWickerath <jan.wickerath@mailbox.org>
2017-01-18 18:22:59 +0100

    Testcase for tauchen algorithm.

--- Assignment3/Ayiagari/my_bewley_model_test.py
+++ Assignment3/Ayiagari/my_bewley_model_test.py
@@ -1,9 +1,17 @@
 import unittest
+import numpy as np
 from my_bewley_model import MyBewleyModel
 
 
 class TestMyBewleyModel(unittest.TestCase):
-    pass
+
+    def test_tauchen(self):
+        bewley = MyBewleyModel(r=.01)
+        transit = bewley._tauchen(rho=.5, sigma=1)
+        true_sol = np.array([[.6915, .3085], [.5398, .4602]])
+        np.testing.assert_array_almost_equal(
+            transit, true_sol, decimal=3
+        )
 
 
 if __name__ == '__main__':
