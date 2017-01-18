20987d58ade64d04151f3d17e6a923c75ca2eb9f
JanWickerath <jan.wickerath@mailbox.org>
2017-01-18 19:01:35 +0100

    Implement tauchen algorithm.

--- Assignment3/Ayiagari/my_bewley_model.py
+++ Assignment3/Ayiagari/my_bewley_model.py
@@ -4,12 +4,13 @@ style economy.
 
 """
 
-# import numpy as np
+import numpy as np
+from scipy.stats import norm
 
 
 class MyBewleyModel():
     """docstring for MyBewleyModel."""
-    def __init__(self, r):
+    def __init__(self, r=.02):
         pass
 
     def _tauchen(self, rho, sigma, n=2, sup_low=0.2, sup_up=1):
@@ -19,33 +20,43 @@ class MyBewleyModel():
         matrix P of the Markov chain.
         """
         # 1. Choice of points
-        # Compute unconditional standard deviation of dependent variable
-
-        # Compute the largest point as multiple m of this standard deviation
-
-        # Set the first point as the negative of the largest point
-
+        # Set the first point equal to sup_low and the last point equal to
+        # sup_up.
         # Set the grid for states as n linearly spaced points between the
-        # largest and smallest points calculated as above.
-
+        # largest and smallest points as defined above.
         # Calculate the distance between two successive points in the state
-        # space and store this distance in variable d.
+        # space and store this distance in variable dist.
+        states, dist = np.linspace(sup_low, sup_up, n, retstep=True)
 
         # 2. Compute transition matrix
         # Initialize transition matrix of size nxn.
+        transit = np.array([[np.nan] * n] * n)
 
-        # Compute first column of the transition matrix
-        # P(x_i, x_0) = F(x_0 - rho*x_i + d/2) for all i
+        for idx in range(n):
+            # Compute first column of the transition matrix
+            # P(x_i, x_0) = F(x_0 - rho*x_i + d/2) for all i
+            transit[idx, 0] = norm.cdf(states[0] - rho * states[idx] + dist/2)
 
-        # Compute last column of the transition matrix
-        # P(x_i, x_n-1) = 1 - F(x_n-1 - rho*x_i - d/2)
+            # Compute last column of the transition matrix
+            # P(x_i, x_n-1) = 1 - F(x_n-1 - rho*x_i - d/2)
+            transit[idx, n-1] = 1 - norm.cdf(
+                states[n-1] - rho * states[idx] - dist / 2
+            )
 
         # Compute remaining columns according to
         # P(x_i, x_j) = F(x_j - rho*x_i + d/2) - F(x_j - rho*x_i - d/2)
+        if n > 2:
+            for row_idx in range(0, n):
+                for col_idx in range(1, n - 1):
+                    transit[row_idx, col_idx] = norm.cdf(
+                        states[col_idx] - rho * states[row_idx] + dist/2
+                    ) - \
+                    norm.cdf(
+                        states[col_idx] - rho * states[row_idx] - dist/2
+                    )
 
         # Return state vector and transition matrix
-
-        pass
+        return transit, states
 
     def _utility(self):
         pass
