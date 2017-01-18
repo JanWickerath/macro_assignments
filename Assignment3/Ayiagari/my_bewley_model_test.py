8f24a58481bcd28b6538766438ef7c32a7759cbb
JanWickerath <jan.wickerath@mailbox.org>
2017-01-18 19:00:49 +0100

    Adjust tauchen tests for new output.

--- Assignment3/Ayiagari/my_bewley_model_test.py
+++ Assignment3/Ayiagari/my_bewley_model_test.py
@@ -7,7 +7,7 @@ class TestMyBewleyModel(unittest.TestCase):
 
     def test_tauchen_len2(self):
         bewley = MyBewleyModel(r=.01)
-        transit = bewley._tauchen(rho=.5, sigma=1)
+        transit = bewley._tauchen(rho=.5, sigma=1)[0]
         true_sol = np.array([[.6915, .3085], [.5398, .4602]])
         np.testing.assert_array_almost_equal(
             transit, true_sol, decimal=3
@@ -15,7 +15,7 @@ class TestMyBewleyModel(unittest.TestCase):
 
     def test_tauchen_len3_rowsums(self):
         bewley = MyBewleyModel(r=.01)
-        transit = bewley._tauchen(rho=.5, sigma=1, n=3)
+        transit = bewley._tauchen(rho=.5, sigma=1, n=3)[0]
         row_sums = []
         for idx in range(len(transit)):
             row_sums.append(sum(transit[idx]))
