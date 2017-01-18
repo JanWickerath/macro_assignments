adbbf107eec93b1d724bf991f2758f967dad535e
JanWickerath <jan.wickerath@mailbox.org>
2017-01-18 18:56:54 +0100

    Add test for tauchen with three states.

--- Assignment3/Ayiagari/my_bewley_model_test.py
+++ Assignment3/Ayiagari/my_bewley_model_test.py
@@ -5,13 +5,21 @@ from my_bewley_model import MyBewleyModel
 
 class TestMyBewleyModel(unittest.TestCase):
 
-    def test_tauchen(self):
+    def test_tauchen_len2(self):
         bewley = MyBewleyModel(r=.01)
         transit = bewley._tauchen(rho=.5, sigma=1)
         true_sol = np.array([[.6915, .3085], [.5398, .4602]])
         np.testing.assert_array_almost_equal(
             transit, true_sol, decimal=3
         )
+
+    def test_tauchen_len3_rowsums(self):
+        bewley = MyBewleyModel(r=.01)
+        transit = bewley._tauchen(rho=.5, sigma=1, n=3)
+        row_sums = []
+        for idx in range(len(transit)):
+            row_sums.append(sum(transit[idx]))
+        self.assertEqual(row_sums, [1, 1, 1])
 
 
 if __name__ == '__main__':
