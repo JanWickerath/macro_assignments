7e91573a3001bc9b2e75a4fe3c0742dddd582451
JanWickerath <jan.wickerath@mailbox.org>
2017-01-19 15:21:38 +0100

    Add testcase for stationary distributions of states.

--- Assignment3/Ayiagari/my_bewley_model_test.py
+++ Assignment3/Ayiagari/my_bewley_model_test.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from my_bewley_model import tauchen
+from my_bewley_model import MyBewleyModel, tauchen
 
 
 class TestMyBewleyModel(unittest.TestCase):
@@ -18,6 +18,16 @@ class TestMyBewleyModel(unittest.TestCase):
         for idx in range(len(transit)):
             row_sums.append(sum(transit[idx]))
         self.assertEqual(row_sums, [1, 1, 1])
+
+    def test_stationary_stoch_states(self):
+        bewley = MyBewleyModel(
+            r=.02, rho=.5, sigma=1, n_stoch=2, nsup_low=.2, nsup_up=1
+        )
+        stationary = bewley.get_stat_states()
+        true_sol = np.array([[.70710678, -.4961909]])
+        np.testing.assert_array_almost_equal(
+            stationary, true_sol
+        )
 
 
 if __name__ == '__main__':
