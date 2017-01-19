3ca490b35912482335f1314f8316bd8c345b2e28
JanWickerath <jan.wickerath@mailbox.org>
2017-01-19 13:40:01 +0100

    Move implementation of Tauchen algorithm outside model class.

--- Assignment3/Ayiagari/my_bewley_model.py
+++ Assignment3/Ayiagari/my_bewley_model.py
@@ -13,51 +13,6 @@ class MyBewleyModel():
     def __init__(self, r=.02):
         pass
 
-    def _tauchen(self, rho, sigma, n=2, sup_low=0.2, sup_up=1):
-        """Implementation of the Tauchen algorithm to approximate an
-        autoregressive process of order 1 by a Markov chain.
-        Return the discrete vector of states and the transition probability
-        matrix P of the Markov chain.
-        """
-        # 1. Choice of points
-        # Set the first point equal to sup_low and the last point equal to
-        # sup_up.
-        # Set the grid for states as n linearly spaced points between the
-        # largest and smallest points as defined above.
-        # Calculate the distance between two successive points in the state
-        # space and store this distance in variable dist.
-        states, dist = np.linspace(sup_low, sup_up, n, retstep=True)
-
-        # 2. Compute transition matrix
-        # Initialize transition matrix of size nxn.
-        transit = np.array([[np.nan] * n] * n)
-
-        for idx in range(n):
-            # Compute first column of the transition matrix
-            # P(x_i, x_0) = F(x_0 - rho*x_i + d/2) for all i
-            transit[idx, 0] = norm.cdf(states[0] - rho * states[idx] + dist/2)
-
-            # Compute last column of the transition matrix
-            # P(x_i, x_n-1) = 1 - F(x_n-1 - rho*x_i - d/2)
-            transit[idx, n-1] = 1 - norm.cdf(
-                states[n-1] - rho * states[idx] - dist / 2
-            )
-
-        # Compute remaining columns according to
-        # P(x_i, x_j) = F(x_j - rho*x_i + d/2) - F(x_j - rho*x_i - d/2)
-        if n > 2:
-            for row_idx in range(0, n):
-                for col_idx in range(1, n - 1):
-                    transit[row_idx, col_idx] = norm.cdf(
-                        states[col_idx] - rho * states[row_idx] + dist/2
-                    ) - \
-                    norm.cdf(
-                        states[col_idx] - rho * states[row_idx] - dist/2
-                    )
-
-        # Return state vector and transition matrix
-        return transit, states
-
     def _utility(self):
         pass
 
@@ -72,3 +27,49 @@ class MyBewleyModel():
 
     def _aggregates(self):
         pass
+
+
+def tauchen(rho, sigma, n=2, sup_low=0.2, sup_up=1):
+    """Implementation of the Tauchen algorithm to approximate an
+    autoregressive process of order 1 by a Markov chain.
+    Return the discrete vector of states and the transition probability
+    matrix P of the Markov chain.
+    """
+    # 1. Choice of points
+    # Set the first point equal to sup_low and the last point equal to
+    # sup_up.
+    # Set the grid for states as n linearly spaced points between the
+    # largest and smallest points as defined above.
+    # Calculate the distance between two successive points in the state
+    # space and store this distance in variable dist.
+    states, dist = np.linspace(sup_low, sup_up, n, retstep=True)
+
+    # 2. Compute transition matrix
+    # Initialize transition matrix of size nxn.
+    transit = np.array([[np.nan] * n] * n)
+
+    for idx in range(n):
+        # Compute first column of the transition matrix
+        # P(x_i, x_0) = F(x_0 - rho*x_i + d/2) for all i
+        transit[idx, 0] = norm.cdf(states[0] - rho * states[idx] + dist/2)
+
+        # Compute last column of the transition matrix
+        # P(x_i, x_n-1) = 1 - F(x_n-1 - rho*x_i - d/2)
+        transit[idx, n-1] = 1 - norm.cdf(
+            states[n-1] - rho * states[idx] - dist / 2
+        )
+
+    # Compute remaining columns according to
+    # P(x_i, x_j) = F(x_j - rho*x_i + d/2) - F(x_j - rho*x_i - d/2)
+    if n > 2:
+        for row_idx in range(0, n):
+            for col_idx in range(1, n - 1):
+                transit[row_idx, col_idx] = norm.cdf(
+                    states[col_idx] - rho * states[row_idx] + dist/2
+                ) - \
+                norm.cdf(
+                    states[col_idx] - rho * states[row_idx] - dist/2
+                )
+
+    # Return state vector and transition matrix
+    return transit, states
--- Assignment3/Ayiagari/my_bewley_model_test.py
+++ Assignment3/Ayiagari/my_bewley_model_test.py
@@ -1,21 +1,19 @@
 import unittest
 import numpy as np
-from my_bewley_model import MyBewleyModel
+from my_bewley_model import tauchen
 
 
 class TestMyBewleyModel(unittest.TestCase):
 
     def test_tauchen_len2(self):
-        bewley = MyBewleyModel(r=.01)
-        transit = bewley._tauchen(rho=.5, sigma=1)[0]
+        transit = tauchen(rho=.5, sigma=1)[0]
         true_sol = np.array([[.6915, .3085], [.5398, .4602]])
         np.testing.assert_array_almost_equal(
             transit, true_sol, decimal=3
         )
 
     def test_tauchen_len3_rowsums(self):
-        bewley = MyBewleyModel(r=.01)
-        transit = bewley._tauchen(rho=.5, sigma=1, n=3)[0]
+        transit = tauchen(rho=.5, sigma=1, n=3)[0]
         row_sums = []
         for idx in range(len(transit)):
             row_sums.append(sum(transit[idx]))
