0de75152f45531dd9f866aed1efb7b8dc48d752d
JanWickerath <jan.wickerath@mailbox.org>
2017-01-21 14:28:00 +0100

    Implement binary search.

--- Assignment3/Aiyagari/discrete_dyn_prog.py
+++ Assignment3/Aiyagari/discrete_dyn_prog.py
@@ -17,13 +17,26 @@ def _bin_search_opt(fun, sup_grid):
 
     """
     # Initialize minimum and maximum indices of the support grid.
+    idx_min = 0
+    idx_max = len(sup_grid) - 1
 
     # While i_max - i_min > 2
-
+    while (idx_max - idx_min) > 2:
         # Select two indices i_l=floor((i_min + i_max)/2) and i_u=i_l + 1
+        idx_l = np.floor((idx_min + idx_max) / 2)
+        idx_u = idx_l + 1
 
         # if f(x_iu) > f(x_il) set i_min=i_l.
         # else set i_max=i_u
+        if fun(sup_grid[int(idx_u)]) > fun(sup_grid[int(idx_l)]):
+            idx_min = idx_l
+        else:
+            idx_max = idx_u
 
-    # Return the largest element from f(x_imin), f(x_imin+1), f(x_imax)
-    pass
+    # Return the largest element from f(x_imin), f(x_imax)
+    if fun(sup_grid[int(idx_min)]) > fun(sup_grid[int(idx_min + 1)]):
+        return fun(sup_grid[int(idx_min)]), sup_grid[int(idx_min)]
+    elif fun(sup_grid[int(idx_min + 1)]) > fun(sup_grid[int(idx_max)]):
+        return fun(sup_grid[int(idx_min + 1)]), sup_grid[int(idx_min + 1)]
+    else:
+        return fun(sup_grid[int(idx_max)]), sup_grid[int(idx_max)]
