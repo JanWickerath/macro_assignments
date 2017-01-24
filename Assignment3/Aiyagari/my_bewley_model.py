1118d936746fa9091cb777eff34f095aa5e6c6b5
JanWickerath <jan.wickerath@mailbox.org>
2017-01-24 15:48:16 +0100

    Add sigma to tauchen algorithm.

--- Assignment3/Aiyagari/my_bewley_model.py
+++ Assignment3/Aiyagari/my_bewley_model.py
@@ -127,12 +127,14 @@ def tauchen(rho, sigma, n=2, sup_low=0.2, sup_up=1):
     for idx in range(n):
         # Compute first column of the transition matrix
         # P(x_i, x_0) = F(x_0 - rho*x_i + d/2) for all i
-        transit[idx, 0] = norm.cdf(states[0] - rho * states[idx] + dist/2)
+        transit[idx, 0] = norm.cdf(states[0] - rho * states[idx] + dist/2,
+                                   scale=sigma)
 
         # Compute last column of the transition matrix
         # P(x_i, x_n-1) = 1 - F(x_n-1 - rho*x_i - d/2)
         transit[idx, n-1] = 1 - norm.cdf(
-            states[n-1] - rho * states[idx] - dist / 2
+            states[n-1] - rho * states[idx] - dist / 2,
+            scale=sigma
         )
 
     # Compute remaining columns according to
@@ -141,10 +143,12 @@ def tauchen(rho, sigma, n=2, sup_low=0.2, sup_up=1):
         for row_idx in range(0, n):
             for col_idx in range(1, n - 1):
                 transit[row_idx, col_idx] = norm.cdf(
-                    states[col_idx] - rho * states[row_idx] + dist/2
+                    states[col_idx] - rho * states[row_idx] + dist/2,
+                    scale=sigma
                 ) - \
                 norm.cdf(
-                    states[col_idx] - rho * states[row_idx] - dist/2
+                    states[col_idx] - rho * states[row_idx] - dist/2,
+                    scale=sigma
                 )
 
     # Return state vector and transition matrix
