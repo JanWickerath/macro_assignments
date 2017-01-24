d6c040d65ee78e3b9030aeea48772235537c1999
JanWickerath <jan.wickerath@mailbox.org>
2017-01-24 17:18:32 +0100

    Add testcase for transition matrix of combined states.

--- Assignment3/Aiyagari/my_bewley_model_test.py
+++ Assignment3/Aiyagari/my_bewley_model_test.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
-import matplotlib.pyplot as plt
-from my_bewley_model import MyBewleyModel, tauchen
+# import matplotlib.pyplot as plt
+from my_bewley_model import MyBewleyModel, tauchen, transistor
 
 
 class TestMyBewleyModel(unittest.TestCase):
@@ -37,23 +37,46 @@ class TestMyBewleyModel(unittest.TestCase):
         bewley.get_stat_states()
         self.assertAlmostEqual(bewley.avg_labor_sup(), .6)
 
-    def test_solve_visually(self):
-        assets=np.linspace(0, 30, 100)
-        bewley = MyBewleyModel(
-            r=.02, rho=.5, sigma=1, assets=assets
+    # def test_solve_visually(self):
+    #     assets=np.linspace(0, 30, 100)
+    #     bewley = MyBewleyModel(
+    #         r=.02, rho=.5, sigma=1, assets=assets
+    #     )
+    #     v, pol = bewley._solve(1)
+
+    #     plt.figure()
+    #     plt.subplot(1, 2, 1)
+    #     plt.plot(assets, v)
+    #     plt.title('Value function')
+
+    #     plt.subplot(1, 2, 2)
+    #     plt.plot(assets, pol)
+    #     plt.title('Policy function')
+
+    #     plt.show()
+
+    def test_transistor_3states_2shocks(self):
+        policy = np.array([
+            [0, 1],
+            [1, 1],
+            [1, 2]
+        ])
+        stoch_trans = np.array([
+            [.7, .3],
+            [.4, .6]
+        ])
+        true_sol = np.array([
+            [.7, .3, 0, 0, 0, 0],
+            [0, 0, .4, .6, 0, 0],
+            [0, 0, .7, .3, 0, 0],
+            [0, 0, .4, .6, 0, 0],
+            [0, 0, .7, .3, 0, 0],
+            [0, 0, 0, 0, .4, .6]
+        ])
+        trans = transistor(policy, stoch_trans)
+        np.testing.assert_array_equal(
+            trans, true_sol
         )
-        v, pol = bewley._solve(1)
-
-        plt.figure()
-        plt.subplot(1, 2, 1)
-        plt.plot(assets, v)
-        plt.title('Value function')
-
-        plt.subplot(1, 2, 2)
-        plt.plot(assets, pol)
-        plt.title('Policy function')
-
-        plt.show()
 
 
 if __name__ == '__main__':
