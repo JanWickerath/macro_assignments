932b4f45665d10b657c68ea267f6212c50ded71b
JanWickerath <jan.wickerath@mailbox.org>
2017-01-26 18:09:04 +0100

    Create supply demand plot.

--- Assignment3/Aiyagari/assignment3_master.py
+++ Assignment3/Aiyagari/assignment3_master.py
@@ -51,4 +51,50 @@ def plot_asset_dist():
     fig.savefig('figures/asset_distribution.pdf')
 
 
+# Calculate Asset supply and demand for different interest rates.
+def supply_demand_plot():
+    # Initialize interest rate grid supply and demand vectors
+    r_grid = np.linspace(-0.04, 0.06, 100)
+    supply = np.array([np.nan] * len(r_grid))
+    demand = np.array([np.nan] * len(r_grid))
+
+    # Initialize parameters
+    rho = .9
+    sigma = 2
+    assets = np.linspace(0, 30, 100)
+    beta = .95
+    n_stoch = 2
+    alpha = .5
+    delta = .05
+
+    # Loop over interest rate grid and fill supply and demand vectors
+    # appropriately
+    for idx in range(len(r_grid)):
+        r = r_grid[idx]
+        # Create an instance of the Bewley model with given
+        # parameterization
+        bewley = MyBewleyModel(
+            r, rho, sigma, assets, beta, n_stoch, alpha=alpha, delta=delta
+        )
+        bewley.solve_model()
+
+        supply[idx] = bewley.get_asset_supply()
+
+        # Create aggregate demand
+        labor_sup = bewley.get_avg_lab_sup()
+        demand[idx] = aggregate_demand(r, labor_sup, alpha, delta)
+
+    # Plot results
+    fig = plt.figure()
+    plt.plot(supply, r_grid, label='Supply')
+    plt.plot(demand, r_grid, label='Demand')
+
+    plt.legend()
+    plt.xlim(xmax=30)
+    plt.tight_layout()
+    # fig.savefig('figures/supply_demand_plot.pdf')
+    plt.show()
+
+# Solve the exercise.
 plot_asset_dist()
+# supply_demand_plot()
