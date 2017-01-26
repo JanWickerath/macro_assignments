9da014ff456f47a975f998697c7a9c608dc73fcc
JanWickerath <jan.wickerath@mailbox.org>
2017-01-26 16:37:05 +0100

    Move plot of asset grid into function.

--- Assignment3/Aiyagari/assignment3_master.py
+++ Assignment3/Aiyagari/assignment3_master.py
@@ -2,48 +2,53 @@ from my_bewley_model import MyBewleyModel, aggregate_demand
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Set parameters
-r = .01
-rho = .9
-sigma = 2
-assets = np.linspace(0, 30, 100)
-beta = .95
-n_stoch = 2
-alpha = .5
-delta = .05
 
-# Create an instance of the Bewley model with given parameterization
-bewley_small = MyBewleyModel(
-    r, rho, sigma, assets, beta, n_stoch, alpha=alpha, delta=delta
-)
+def plot_asset_dist():
+    # Set parameters
+    r = .01
+    rho = .9
+    sigma = 2
+    assets = np.linspace(0, 30, 100)
+    beta = .95
+    n_stoch = 2
+    alpha = .5
+    delta = .05
 
-# Solve the model and store results internally
-bewley_small.solve_model()
+    # Create an instance of the Bewley model with given parameterization
+    bewley_small = MyBewleyModel(
+        r, rho, sigma, assets, beta, n_stoch, alpha=alpha, delta=delta
+    )
 
-# Do the same of a larger model
-# Set parameters
-n_stoch_large = 32
+    # Solve the model and store results internally
+    bewley_small.solve_model()
 
-# Create an instance of the Bewley model with given parameterization
-bewley_large = MyBewleyModel(
-    r, rho, sigma, assets, beta, n_stoch_large, alpha=alpha, delta=delta
-)
+    # Do the same of a larger model
+    # Set parameters
+    n_stoch_large = 32
 
-# Solve the model and store results internally
-bewley_large.solve_model()
+    # Create an instance of the Bewley model with given parameterization
+    bewley_large = MyBewleyModel(
+        r, rho, sigma, assets, beta, n_stoch_large, alpha=alpha, delta=delta
+    )
+
+    # Solve the model and store results internally
+    bewley_large.solve_model()
 
 
 
-# Plot distribution of assets
-fig = plt.figure()
-plt.subplot(1, 2, 1)
-plt.plot(assets, bewley_small.get_stat_assets())
-plt.title('2 Stochastic states')
+    # Plot distribution of assets
+    fig = plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.plot(assets, bewley_small.get_stat_assets())
+    plt.title('2 Stochastic states')
 
-plt.subplot(1, 2, 2)
-plt.plot(assets, bewley_large.get_stat_assets())
-plt.title('32 Stochastic states')
+    plt.subplot(1, 2, 2)
+    plt.plot(assets, bewley_large.get_stat_assets())
+    plt.title('32 Stochastic states')
 
-plt.tight_layout()
+    plt.tight_layout()
 
-fig.savefig('figures/asset_distribution.pdf')
+    fig.savefig('figures/asset_distribution.pdf')
+
+
+plot_asset_dist()
